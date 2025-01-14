489bd47dcdcbc7db30986023a8d2638a2684741b
Ariunaa Bayarmagnai <arrrrrriun@outlook.com>
2025-01-14 15:59:51 +0800

    Side navbar styling completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
+import React, { useState } from 'react';
 import './styles.css'
+
 
 function App() {
 
+  const [isOpen, setIsOpen] = useState(true); // State to toggle navbar
+  const [isShown, setisShown] = useState(true);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  const showProjects = () => {
+    setisShown(!isShown);
+  }
+
   return (
-    <div className="sideNav">
-      <div className="sideNavTop">
-        <div className='sideNavTopMenu'>
-          <button>A Name v</button>
-          <button>bell</button>
+    <div>
+      <button className={`sideNav-abs ${!isOpen ? 'openBtn' : ''}`} onClick={toggleNavbar}><i className='material-icons-outlined'>view_sidebar</i></button>
+      <div className={`sideNav ${isOpen ? 'open' : ''}`}>
+        <div className='sideNavTop'>
+          <button className='profileBtn'><p>A</p>Ariunaa<i className="material-icons">expand_more</i></button>
+          <i className="material-icons-outlined bell">notifications</i>
         </div>
         <div className="sideNavMenu">
           <li>
-            <ul>Add task</ul>
-            <ul>Search</ul>
-            <ul>Today</ul>
-            <ul>Upcoming</ul>
-            <ul>Filters & Labels</ul>
-            <ul>My Projects</ul>
-            <ul>#Home</ul>
+            <ul className='red'><i className="red material-icons">add_circle</i>Add task</ul>
+            <ul><i className="material-icons">search</i>Search</ul>
+            <ul className='active'><i className="material-icons">inbox</i>Inbox</ul>
+            <ul><i className="material-icons">today</i>Today</ul>
+            <ul><i className="material-icons">calendar_month</i>Upcoming</ul>
+            <ul><i className="material-icons">grid_view</i>Filters & Labels</ul>
+            <button className='projectsBtn'>My Projects<span><button><i className="material-icons">add</i></button><button onClick={showProjects}><i className="material-icons">{isShown ? 'chevron_right' : 'expand_more'}</i></button></span></button>
+            {!isShown && (<li>
+              <ul><i className="material-icons">tag</i>Home</ul>
+            </li>)}
           </li>
         </div>
-      </div>
-      <div className="sideNavBottom">
-        <li>
-          <ul>Add a team</ul>
-          <ul>Browse templates</ul>
-        </li>
+        <div className="sideNavBottom">
+          <li>
+            <ul><i className="material-icons">add</i>Add a team</ul>
+            <ul><i className="material-icons">web</i>Browse templates</ul>
+          </li>
+        </div>
       </div>
     </div>
   )
